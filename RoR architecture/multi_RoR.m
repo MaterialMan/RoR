9852cf0729c1f7afa10b1f58cf7c917168b34b39
md596 <md596@ELECPC244.its.york.ac.uk>
2018-02-07 17:46:44 +0000

    More updates:
    Working free-flow design

--- RoR architecture/multi_RoR.m
+++ RoR architecture/multi_RoR.m
@@ -1,30 +1,22 @@
 % Make sure files are on the path
 addpath(genpath('\RoR-master-Git\'))
-
-dataSet = 'Laser'
-resType = 'RoR';
-maxMinorUnits=100;
-maxMajorUnits=1;
-evolve_RoR
-
-
-%% 
+ 
 clear
 rng(10,'twister');
 dataSet = 'NARMA10'
 resType = 'RoR_IA';
 maxMinorUnits=25;
-maxMajorUnits=4;
+maxMajorUnits=8;
 
 %Evolutionary parameters
 numTests = 10;
 popSize =15;           
-numEpoch = 100;
+numEpoch = 250;
 numMutate = 0.3; 
 deme = popSize-1;   
 recRate = 0.4; 
 rankedFitness = 0;
-startFull = 1;
+startFull = 0;
 leakOn = 1;
 genPrint = 2;
 
